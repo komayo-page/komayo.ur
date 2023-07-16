62b8ababd3c1356b12693013632c4ddf4f222dff
Cyril Kato <contact@cyril.email>
2023-07-16 16:15:12 +0200

    feat: new version

--- service-worker.js
+++ service-worker.js
@@ -1,23 +1,22 @@
 self.addEventListener('install', (event) => {
-  event.waitUntil(
-    caches.open('v1').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/css/pico.classless.min.css',
-        '/favicon-512x512.png',
-        '/favicon.svg',
-        '/manifest.json',
-        '/soye-hue-mukhalif-par-muskura-raha-hai.png',
-        '/soye-hue-mukhalif-par-muskura-raha-hai.webp',
-      ]);
-    })
-  );
+event.waitUntil(
+caches.open('v1').then((cache) => {
+return cache.addAll([
+'/',
+'/css/pico.classless.min.css',
+'/favicon-512x512.png',
+'/favicon.svg',
+'/manifest.json',
+'/soye-hue-mukhalif-par-muskura-raha-hai.png',
+'/soye-hue-mukhalif-par-muskura-raha-hai.webp',
+]);
+})
+);
 });
-
 self.addEventListener('fetch', (event) => {
-  event.respondWith(
-    caches.match(event.request).then((response) => {
-      return response || fetch(event.request);
-    })
-  );
+event.respondWith(
+caches.match(event.request).then((response) => {
+return response || fetch(event.request);
+})
+);
 });
